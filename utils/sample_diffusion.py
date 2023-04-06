162b281b92a9827a528af634b248581b9ef5d30c
Xinxi Zhang <79646025+CQUEEN-lpy@users.noreply.github.com>
2023-04-06 17:24:31 -0400

    update

--- utils/sample_diffusion.py
+++ utils/sample_diffusion.py
@@ -68,18 +68,6 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
-
-
-
-"""pipe = StableDiffusionPipeline.from_pretrained(config.model_id, torch_dtype=torch.float32)
-pipe = pipe.to("cuda")
-
-prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]
-
-image.save("astronaut_rides_horse.png")"""
-
 if __name__ == '__main__':
     config = parse_args()
     class_dict = json.load(open(config.class_json, 'r'))
