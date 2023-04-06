1bd4eef0ce69315b8a60b9e65a723adc096c7cfa
Xinxi Zhang <xz657@scarletmail.rutgers.edu>
2023-04-06 10:41:43 -0400

    Create class.json

---
+++ test.py
@@ -0,0 +1,14 @@
+import json
+
+with open('./data/class.info', 'r') as f:
+    dict = {}
+    for line in f:
+        line = line[:-1] if '\n' in line else line
+        subject, cls = line.split(',')
+        dict[subject] = cls
+
+with open('./data/class.json', 'w') as f:
+    json.dump(dict, f)
+
+
+
