3cbf656d8ca3b08cd140337145019b54465ab0b0
Xinxi Zhang <79646025+CQUEEN-lpy@users.noreply.github.com>
2023-04-07 21:14:43 -0400

    update create_simple_prompts.py

--- utils/create_simple_prompts.py
+++ utils/create_simple_prompts.py
@@ -21,3 +21,36 @@ with open('../data/prompt_simple.json', 'w') as f:
             prompt_dict[cls].append(f'a [V] {cls}')
 
     json.dump(prompt_dict, f)
+
+
+def create_simple_prompt_real():
+    with open('../data/class.json', 'r') as f:
+        class_dict = json.load(f)
+
+    with open('../data/prompt_simple_real.json', 'w') as f:
+
+        subject_path = '../data/img/subject'
+        prompt_dict = {}
+
+        for i in os.listdir(subject_path):
+            try:
+                cls = class_dict[i]
+            except:
+                continue
+            prompt_dict[cls] = []
+
+            for file in os.listdir(os.path.join(subject_path, i)):
+                if '.jpg' in file:
+                    path = os.path.join(subject_path, i, file)
+                    item = {
+                        'img_path': "/".join(os.path.normpath(path).split(os.sep)[-3:]),
+                        'prompt': f'a [V] {cls}'
+                    }
+
+                    prompt_dict[cls].append(item)
+
+        json.dump(prompt_dict, f)
+
+
+if __name__ == '__main__':
+    create_simple_prompt_real()
