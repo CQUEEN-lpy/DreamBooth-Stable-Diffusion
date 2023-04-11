38fffded5dda608374d8182f74af2e564d0fce6c
CQueen-lpy <79646025+CQUEEN-lpy@users.noreply.github.com>
2023-04-11 05:04:53 +0000

    update

--- utils/DreamBooth_Dataset.py
+++ utils/DreamBooth_Dataset.py
@@ -1,6 +1,6 @@
 import math
 import os.path
-
+import datasets
 from torch.utils import data
 import json
 from PIL import Image
@@ -8,55 +8,41 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-class DreamBooth_Dataset(data.Dataset):
-    def __init__(self, real_json, generated_json, subject, root_path, transform=None):
 
-        self.real_list = json.load(open(real_json, 'r'))[subject]
-        self.generated_list = json.load(open(generated_json, 'r'))[subject]
-        self.transform = transform
-        self.root_path = root_path
+def get_dataset(real_json, generated_json, subject, root_path, transform=None):
 
-        self.real_images = []
-        self.real_prompts = []
-        for item in self.real_list:
-            real_img = Image.open(os.path.join(self.root_path, item['img_path'])).convert("RGB")
-            self.real_images.append(real_img)
-            self.real_prompts.append(item['prompt'])
+    real_list = json.load(open(real_json, 'r'))[subject]
+    generated_list = json.load(open(generated_json, 'r'))[subject]
 
-        self.generated_images = []
-        self.generated_prompts = []
-        for item in self.real_list:
-            generated_img = Image.open(os.path.join(self.root_path, item['img_path'])).convert("RGB")
-            self.generated_images.append(generated_img)
-            self.generated_prompts.append(item['prompt'])
+    for i in range(len(real_list)):
+        real_list[i]['img_path'] = os.path.join(root_path, real_list[i]['img_path'])
 
-        repeat_time = int(math.ceil(len(self.generated_prompts) / len(self.real_images)))
-        self.real_images = self.real_images * repeat_time
-        self.real_images = self.real_images[: len(self.generated_images)]
-        self.real_prompts = self.real_prompts * repeat_time
-        self.real_prompts = self.real_prompts[: len(self.generated_images)]
+    for i in range(len(generated_list)):
+        generated_list[i]['img_path'] = os.path.join(root_path, generated_list[i]['img_path'])
 
-    def __len__(self):
-        return len(self.generated_list)
+    repeat_time = int(math.ceil(len(generated_list) / len(real_list)))
+    real_list = real_list * repeat_time
+    real_list = real_list[: len(generated_list)]
 
-    def __getitem__(self, index):
+    dataset_dict = {
+        'real_image': list(item['img_path'] for item in real_list),
+        'real_prompt': list(item['prompt'] for item in real_list),
+        'generated_image': list(item['img_path'] for item in generated_list),
+        'generated_prompt': list(item['prompt'] for item in generated_list)
+    }
 
-        real_imgs = self.real_images[index]
-        generated_imgs = self.generated_images[index]
+    features = datasets.Features({
+        'real_image': datasets.Image(),
+        'real_prompt': datasets.Value('string'),
+        'generated_image': datasets.Image(),
+        'generated_prompt': datasets.Value('string'),
+    })
 
-        if self.transform:
-            real_imgs = self.transform(real_imgs)
-            generated_imgs = self.transform(generated_imgs)
+    dataset = datasets.Dataset.from_dict(dataset_dict, features, split="train")
 
-        real_prompts = self.real_prompts[index]
-        generated_prompts = self.generated_prompts[index]
+    return dataset
 
-        return {
-            'real_img': real_imgs,
-            'real_prompt': real_prompts,
-            'generated_img': generated_imgs,
-            'generated_prompt': generated_prompts,
-        }
+def collate_fn(item):
 
 
 if __name__ == '__main__':
@@ -69,15 +55,19 @@ if __name__ == '__main__':
         ]
     )
 
-    dataset = DreamBooth_Dataset('../data/prompt_simple_real.json', '../data/prompt_simple_generated.json', 'dog6', transform=preprocess, root_path='../data/img')
-
+    dataset = get_dataset('../data/prompt_simple_real.json', '../data/prompt_simple_generated.json', 'dog6', '../data/img', transform = preprocess)
     fig, axs = plt.subplots(1, 4, figsize=(16, 4))
     indexs = np.random.randint(0, len(dataset), 4)
     sample_data = dataset[indexs]
     for i in range(len(indexs)):
-        axs[i].imshow(sample_data['real_img'][i][0])
+        axs[i].imshow(sample_data['real_image'][i])
         axs[i].set_axis_off()
-        axs[i].text(0.5, -0.2, sample_data['real_prompt'][i][1], fontsize=10, ha='center', transform=axs[i].transAxes)
+        axs[i].text(0.5, -0.2, sample_data['real_prompt'][i], fontsize=10, ha='center', transform=axs[i].transAxes)
+
+    plt.show()
+
+
+
 
 
 
