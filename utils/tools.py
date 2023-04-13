fb855dea1daf3ef0d28aa3ded02dc80ec114af0c
Xinxi Zhang <79646025+CQUEEN-lpy@users.noreply.github.com>
2023-04-13 12:19:51 -0400

    update

--- utils/tools.py
+++ utils/tools.py
@@ -1,4 +1,6 @@
-class prompt_generator():
-    def __init__(self, identifier, cls):
-        self.identifier = identifier
-        self.cls = cls
+import matplotlib.pyplot as plt
+def test_generated_imgs(item):
+    fig, axs = plt.subplots(1, 8, figsize=(16, 4))
+    for i, image in enumerate(item):
+        axs[i].imshow(image.permute(1, 2, 0).numpy() / 2 + 0.5)
+        axs[i].set_axis_off()
